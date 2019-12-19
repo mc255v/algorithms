c0c386fbe52dc154743e68bb2c20a2b022c43509
Matthew Cook <mc255v@gmail.com>
2019-12-19 12:49:23 +0900

    created BST starter files

---
+++ cs-review/BinarySearchTree.js
@@ -0,0 +1,9 @@
+class Node {
+
+}
+
+class BinarySearchTree {
+
+}
+
+module.exports = BinarySearchTree;
---
+++ cs-review/tests/binarysearchtree.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+const BST = require('../BinarySearchTree');
+
+describe("BinarySearchTree", () => {
+
+  describe("", () => {
+  
+  });
+});
