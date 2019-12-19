fd297b915dc9d540947c3cf5ef40655ac260c1f2
Matthew Cook <mc255v@gmail.com>
2019-12-19 16:37:16 +0900

    implemented first half of BST

--- cs-review/BinarySearchTree.js
+++ cs-review/BinarySearchTree.js
@@ -1,8 +1,61 @@
 class Node {
-
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return this;
+
+      if (value < current.value) {
+        if (current.left) {
+          current = current.left
+        } else {
+          current.left = newNode;
+          return this;
+        }
+      } else {
+        if (current.right) {
+          current = current.right;
+        } else {
+          current.right = newNode;
+          return this;
+        }
+      }
+    }
+  }
+
+  find(value) {
+    if (!this.root) return null;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    return current;
+  }
+
+  
 
 }
 
--- cs-review/tests/binarysearchtree.js
+++ cs-review/tests/binarysearchtree.js
@@ -1,9 +1,94 @@
 const { expect } = require('chai');
 const BST = require('../BinarySearchTree');
 
-describe("BinarySearchTree", () => {
+describe.only("BinarySearchTree", () => {
+  // Assuming no duplicates allowed
+  describe("insert", () => {
+    it("should insert a node and set as root for empty BST", () => {
+      const bst = new BST();
 
-  describe("", () => {
-  
+      bst.insert(10);
+
+      expect(bst.root.value).to.equal(10);
+    });
+
+    it("should insert in the proper place", () => {
+      const bst = new BST();
+
+      bst.insert(10);
+      bst.insert(8);
+      bst.insert(11);
+      bst.insert(9);
+
+      expect(bst.root.value).to.equal(10);
+      expect(bst.root.left.value).to.equal(8);
+      expect(bst.root.left.left).to.be.null;
+      expect(bst.root.left.right.value).to.equal(9);
+      expect(bst.root.right.value).to.equal(11);
+    });
+
+    it("should not insert duplicate values or modify BST", () => {
+      const bst = new BST();
+
+      bst.insert(10);
+      const compare = bst.insert(8);
+      const duplicate = bst.insert(10);
+
+      expect(compare).to.deep.equal(duplicate);
+    });
+  });
+
+  describe("find", () => {
+    it("should return null for an empty BST", () => {
+      const bst = new BST();
+
+      const found = bst.find(5);
+
+      expect(found).to.be.null;
+    });
+
+    it("should return the node if found", () => {
+      const bst = new BST();
+
+      /*
+            10
+          8   11
+        7 9     13
+      5
+      */
+
+      bst.insert(10);
+      bst.insert(8);
+      bst.insert(11);
+      bst.insert(7);
+      bst.insert(9);
+      bst.insert(5);
+      bst.insert(13);
+
+      const found = bst.find(9);
+
+      expect(found.value).to.equal(9);
+    });
+
+    it("should return null if node not found", () => {
+      const bst = new BST();
+
+      /*
+            10
+          8   11
+        7 9     13
+      5
+      */
+
+      bst.insert(10);
+      bst.insert(8);
+      bst.insert(11);
+      bst.insert(7);
+      bst.insert(9);
+      bst.insert(5);
+      bst.insert(13);
+
+      expect(bst.find(12)).to.be.null;
+    });
   });
 });
