54df1c9f10b8ff0e700e3add154e6cef6dfcb721
Matthew Cook <mc255v@gmail.com>
2019-12-31 18:55:31 +0900

    added mergeTwoLists

--- leetcode/easy/Easy1-10.js
+++ leetcode/easy/Easy1-10.js
@@ -92,4 +92,27 @@ module.exports = {
     return false;
   },
 
+  mergeTwoLists: (l1, l2) => {
+    const result = new ListNode(0);
+    let current = result;
+    while(l1 || l2) {
+      if (l2 && (!l1 || l1.val > l2.val)) {
+        current.next = new ListNode(l2.val);
+        l2 = l2.next;
+      } else {
+        current.next = new ListNode(l1.val);
+        l1 = l1.next;
+      }
+      current = current.next;
+    }
+    return result.next;
+  },
+
+}
+
+class ListNode {
+  constructor(val) {
+  this.val = val;
+  this.next = null;
+  }
 }
