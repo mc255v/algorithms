d3fc040b3be00b83ba9da1982d1696d0add28416
Matthew Cook <mc255v@gmail.com>
2022-02-24 22:58:10 -0600

    medium array 153

--- LeetCode/Medium/array.js
+++ LeetCode/Medium/array.js
@@ -1,1 +1,28 @@
-module.exports = {};
+module.exports = {
+  //153. Find Minimum in Rotated Sorted Array
+  // review comment: took horribly long to come up with
+  // approach is sloppy but right diretion-ish
+  findMin: (nums) => {
+    const getMiddle = () => Math.floor(nums.length / 2);
+    while (true) {
+      const mid = getMiddle();
+      const end = nums.length - 1;
+
+      if (nums.length === 3 && nums[0] > nums[mid]) {
+        return nums[mid];
+      } else if (nums[0] > nums[mid] && nums[mid] < nums[end]) {
+        if (nums[mid - 1] > nums[mid]) {
+          return nums[mid];
+        } else {
+          nums = nums.splice(0, mid);
+        }
+      } else if (nums[0] < nums[mid] && nums[mid] > nums[end]) {
+        nums = nums.splice(mid);
+      } else if (nums[0] < nums[mid] && nums[mid] <= nums[end]) {
+        return nums[0];
+      } else {
+        return nums[mid];
+      }
+    }
+  },
+};
--- LeetCode/Medium/array.spec.js
+++ LeetCode/Medium/array.spec.js
@@ -1,4 +1,37 @@
 const { expect } = require("chai");
-const Medium = require("./array");
+const MediumArray = require("./array");
 
-describe("LeetCode Medium Array Problems", () => {});
+describe("LeetCode Medium Array Problems", () => {
+  describe("findMin", () => {
+    it("should return smallest number with 1 rotation", () => {
+      const test = [7, 0, 1, 2, 4, 5, 6];
+      const expected = 0;
+
+      expect(MediumArray.findMin(test)).to.equal(expected);
+    });
+    it("should return smallest number with no rotation", () => {
+      const test = [1, 2, 3];
+      const expected = 1;
+
+      expect(MediumArray.findMin(test)).to.equal(expected);
+    });
+    it("should return smallest number with no rotation and gaps between numbers", () => {
+      const test = [11, 13, 15, 17];
+      const expected = 11;
+
+      expect(MediumArray.findMin(test)).to.equal(expected);
+    });
+    it("should return smallest number with no rotation and small array", () => {
+      const test = [1, 2];
+      const expected = 1;
+
+      expect(MediumArray.findMin(test)).to.equal(expected);
+    });
+    it("should return smallest number with 3 rotations", () => {
+      const test = [4, 5, 1, 2, 3];
+      const expected = 1;
+
+      expect(MediumArray.findMin(test)).to.equal(expected);
+    });
+  });
+});
